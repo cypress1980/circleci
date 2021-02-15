3f6a981e45babf77b353dfee96bfce11eae8c5d5
Kailash Pathak <kailash.pathak@3pillarglobal.com>
2021-02-15 20:15:15 +0530

    fail tc

--- cypress/integration/examples/login.spec.js
+++ cypress/integration/examples/login.spec.js
@@ -6,7 +6,7 @@ describe("Test login funcationlity ", () => {
   });
   it("Login Into Application", () => {
     cy.get("#ctl00_CPHContainer_txtUserLogin").type("kailash1980");
-    cy.get("#ctl00_CPHContainer_txtPassword").type("Sonie8088");
+    cy.get("#ctl00_CPHContainer_txtPassword").type("Sonie80881");
     cy.get("#ctl00_CPHContainer_btnLoginn").click();
   });
   it("Logout From Application", () => {
