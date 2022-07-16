c5fb9a09205f8df07e738b80832dc5a9164ab2b0
cypress1980 <76464201+cypress1980@users.noreply.github.com>
2022-07-16 08:05:00 +0530

    Update bstackdemo.spec.js

--- cypress/integration/examples/bstackdemo.spec.js
+++ cypress/integration/examples/bstackdemo.spec.js
@@ -4,12 +4,12 @@ describe("Test login funcationlity : login into  the application and logout ", (
   it("Open URL", () => {
     cy.visit("https://bstackdemo.com/");
   });
-  it("Verify links in Browser Stack Home Page ", () => {
+  it("Verify Links in Browser Stack Home Page ", () => {
     cy.contains("Offers");
     cy.contains("Orders");
     cy.contains("Favourites");
   });
-  it("Verify Phone Modle in Home Page ", () => {
+  it("Verify Phone Models in Browser Stack Home Page ", () => {
     cy.contains("iPhone 12");
     cy.contains("Pixel 2");
     cy.contains("One Plus 8");
